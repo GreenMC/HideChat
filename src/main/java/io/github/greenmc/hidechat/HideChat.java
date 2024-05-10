d57ba2e4151e5bda1d92644ee41a6b5e8bde7c8e
Ali Arslan <aliarslann5500@gmail.com>
2024-05-10 17:30:54 +0300

    Add LibertyBans support

--- src/main/java/io/github/greenmc/hidechat/HideChat.java
+++ src/main/java/io/github/greenmc/hidechat/HideChat.java
@@ -1,17 +1,38 @@
 package io.github.greenmc.hidechat;
 
+import io.github.greenmc.hidechat.hook.HideChatHook;
+import io.github.greenmc.hidechat.hook.impl.EssentialsHook;
+import io.github.greenmc.hidechat.hook.impl.LibertyBansHook;
+import io.github.greenmc.hidechat.hook.impl.LiteBansHook;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HideChat extends JavaPlugin {
 
+    private HideChatHook hook;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        PluginManager pluginManager = getServer().getPluginManager();
+        if (pluginManager.isPluginEnabled("LiteBans")) {
+            hook = new LiteBansHook(this);
+        }
 
+        else if (pluginManager.isPluginEnabled("LibertyBans")) {
+            hook = new LibertyBansHook(this);
+        }
+
+        else if (pluginManager.isPluginEnabled("EssentialsX")) {
+            hook = new EssentialsHook(this);
+        }
+
+        getLogger().info(hook.getClass().getName() + " initialized!");
+        getLogger().info("HideChat v" + getDescription().getVersion() + " now enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
 }
--- src/main/java/io/github/greenmc/hidechat/hook/impl/DefaultHook.java
+++ src/main/java/io/github/greenmc/hidechat/hook/impl/DefaultHook.java
@@ -1,6 +1,18 @@
 package io.github.greenmc.hidechat.hook.impl;
 
+import io.github.greenmc.hidechat.HideChat;
 import io.github.greenmc.hidechat.hook.HideChatHook;
+import org.bukkit.entity.Player;
 
 public class DefaultHook extends HideChatHook {
+
+    public DefaultHook(HideChat plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public boolean isMuted(Player player) {
+        return false;
+    }
+
 }
